c9941c3179f1dcd3ac0f59d64f639bcf709fb127
TheCrether <denisimeri6@gmail.com>
2020-06-14 23:16:23 +0200

    put the right thing into sudoStartup.sh

--- sudoStartup.sh
+++ sudoStartup.sh
@@ -2,9 +2,7 @@
 # max_user_watches_stuff
 export SUDO_ASKPASS=/usr/bin/ksshaskpass
 
-#echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/50-max-user-watches.conf
-#sudo sysctl --system
-echo 512000 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
+echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
 
 # setup locking with xss-lock
 $HOME/scripts/autolock.sh
