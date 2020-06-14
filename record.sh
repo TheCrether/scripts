09c8ee89917f4c40202fe50033354c5aebe97e27
TheCrether <denisimeri6@gmail.com>
2020-06-14 23:14:37 +0200

    added restartplasma scritp

--- record.sh
+++ record.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
+OFFSET="0:0"
+
 case "$1" in
   record)
-	OFFSET="0:0"
 	if [[ $# -lt 5 ]] || [[ $# -gt 6 ]]; then
 	  echo "Wrong number of arguments! Look: at `${0##*/} help` for help"
 	fi
-	if [[ $# -eq 6 ]]; then
-	  OFFSET=$6
-	fi
-	echo $OFFSET
+    if [[ $# -eq 6 ]]; then
+      OFFSET=$6
+    fi
 	ffmpeg -f x11grab -framerate $2 -video_size $3 -i $DISPLAY.$4+$OFFSET $5
+	;;
+  forward)
+	if [[ $# -lt 4 ]] || [[ $# -gt 5 ]]; then
+	  echo "Wrong number of arguments! Look: at `${0##*/} help` for help"
+	fi
+    if [[ $# -eq 5 ]]; then
+      OFFSET=$5
+    fi
+
+	ffmpeg -f x11grab \
+	  -framerate $2\
+	  -video_size $3\
+	  -i $DISPLAY.$4+$OFFSET\
+	  -f opengl "output"
 	;;
 
   help | --help | *)
-	echo "Usage: ${0##*/} [help | record] framerate video_size displaynumber output [offset]"
+	echo "Usage: ${0##*/} [help | record | forward] framerate video_size displaynumber output [offset]"
+	echo "record: ${0##*/} record framerate video_size displaynumber output [offset]"
+	echo "forward: ${0##*/} forward framerate video_size displaynumber [offset]"
 	echo
 	echo "If help or record is not specified, it will default to help"
 	echp "When its time to stop the recording, you need to press Ctrl+C in your terminal, ffmpeg will save the file"
