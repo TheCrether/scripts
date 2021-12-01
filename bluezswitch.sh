852661f3daeeb0ce133ffc0274115be42ac8963e
TheCrether <thecrether@gmail.com>
2021-12-01 22:29:14 +0100

    :fire:sparkles: add dyndns script for vercel domains remove unnecessary files

--- bluezswitch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BLUEZCARD=`pactl list cards short | egrep -o bluez.*[[:space:]]`
-pactl set-card-profile $BLUEZCARD a2dp_sink
-pactl set-card-profile $BLUEZCARD off
-pactl set-card-profile $BLUEZCARD a2dp_sink
---
+++ vercel-dyn-dns.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+# Original code from https://github.com/MattMoony
+
+import json
+import requests as req
+from time import sleep
+from datetime import datetime
+
+
+def get_public_ip():
+    try:
+        status = json.loads(req.get('https://wtfismyip.com/json').text)
+        return status['YourFuckingIPAddress']
+    except req.exceptions.ConnectionError:
+        print('[!] WTFISMYIP: Error - Connection failed!')
+    except json.JSONDecodeError:
+        print('[!] WTFISMYIP: Error - Unexpected response!')
+
+
+def err(msg, text):
+    print(msg)
+    with open('dyn-dns.log', 'a') as f:
+        f.write('[{}]: ERROR UPDATING "{}" {}\n'.format(
+            datetime.now().isoformat(), r['name'], '-'*16))
+        f.write('\t' + text + '\n')
+        f.write('\n')
+
+
+def update_dns_record(ip):
+    global CONF
+    print('='*64)
+    for r in CONF['records']:
+        try:
+            domains = json.loads(
+                req.get(f'https://vercel.com/api/v4/domains/{CONF["domain"]}/records',
+                        headers={'Authorization': "Bearer " + CONF["auth_token"]}).text)
+            if "code" in domains and domains['code'] == "not_found":
+                err('[!] DYN-DNS: Error - Could not update DNS record ... (Logging response) ',
+                    json.dumps(domains))
+                return
+            rec = next(filter(lambda rec: rec["name"] ==
+                       r["name"], domains['records']), None)
+            if rec == None:
+                err(
+                    f'[!] DYN-DNS: Error - Could not find record with name "{r["name"]} ... (Logging response)"', domains)
+                return
+            res = req.patch(f'https://vercel.com/api/v4/domains/records/{rec["id"]}',
+                            headers={'Content-Type': 'application/json',
+                                     'Authorization': "Bearer " + CONF["auth_token"]},
+                            json={'name': r['name'], 'ttl': r['ttl'], 'value': ip, 'type': 'A'})
+            if res.status_code != 200:
+                err('[!] DYN-DNS: Error - Could not update DNS record ... (Logging response) ', res.text)
+            else:
+                print(
+                    '[+] Updated DNS-Record "{}" ... (new-ip: {})'.format(r['name'], ip))
+        except req.exceptions.ConnectionError:
+            print('[!] DYN-DNS: Error - Connection failed!')
+        except json.JSONDecodeError:
+            print('[!] DYN-DNS: Error - Unexpected response!')
+
+
+CONF = {}
+
+
+def main():
+    global CONF
+    with open('./dyn-conf.json', 'r') as f:
+        CONF = json.loads(f.read())
+
+    ip = ''
+    try:
+        while True:
+            cuip = get_public_ip()
+            if cuip != None and cuip != ip:
+                update_dns_record(cuip)
+                ip = cuip
+            sleep(CONF['timeout'])
+    except KeyboardInterrupt:
+        print('[*] Bye bye!')
+
+
+if __name__ == '__main__':
+    main()
